b5a2b43be7b5a6a33018527160914295b4313a76
Ad <DeadLimit@lkr5.onmicrosoft.com>
2023-03-03 12:51:14 +0800

    u

--- tutorials/frontend/vue-apollo/app-boilerplate/src/main.ts
+++ tutorials/frontend/vue-apollo/app-boilerplate/src/main.ts
@@ -1,10 +1,17 @@
 // @ts-ignore
 import App from "./App.vue"
-import { createApp } from "vue"
+import { createApp, provide, h } from "vue"
 import { router } from "./router"
 import authPlugin from "./auth/authPlugin"
+import { DefaultApolloClient } from "@vue/apollo-composable"
+import { apolloClient } from "./apollo-client"
 
-const app = createApp(App)
+const app = createApp({
+    setup() {
+        provide(DefaultApolloClient, apolloClient)
+    },
+    render: () => h(App)
+})
 
 app.use(authPlugin)
 app.use(router)
